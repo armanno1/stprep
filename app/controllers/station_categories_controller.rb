359d67cdf6041b471e436ca94ff8e26990e3089e
Rails Udemy Course <railsudemycourse@gmail.com>
2019-09-07 00:45:48 +0100

    messing with disabled collection_selects, fixed a few form issues

--- app/controllers/station_categories_controller.rb
+++ app/controllers/station_categories_controller.rb
@@ -1,4 +1,6 @@
 class StationCategoriesController < ApplicationController
+  before_action :set_station_category, only: [:edit, :update, :show, :destroy]
+
   def index
     @station_categories = StationCategory.all
   end
@@ -17,12 +19,25 @@ class StationCategoriesController < ApplicationController
     end
   end
 
+  def update
+    if @station_category.update(station_category_params)
+      flash[:success] = "Station category was successfully updated."
+      redirect_to course_path(@station_category.course)
+    else
+      render "edit"
+    end
+  end
+
   def show
   end
 
   private
   def station_category_params
     params.require(:station_category).permit(:name, :course_id)
+  end
+
+  def set_station_category
+    @station_category = StationCategory.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
   get 'my_courses', to: 'courses#my_courses'
-  resources :courses, :stations, :questions, :payments
+  resources :courses, :stations, :questions, :payments, :station_categories
   resources :user_courses, only: [:create]
-  resources :station_categories, except: [:destroy]
   mount StripeEvent::Engine, at: '/stripe-event'
 end
