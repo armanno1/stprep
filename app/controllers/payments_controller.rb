4f9b82fc1cfd0fca6149361898105b2960af8ef3
Rails Udemy Course <railsudemycourse@gmail.com>
2019-09-06 15:26:45 +0100

    fixed payment controller bug (was heroku.com not herokuapp.com, why production test payments ended in SSL error)

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -14,8 +14,8 @@ class PaymentsController < ApplicationController
         currency: 'gbp',
         quantity: 1,
       }],
-      success_url: 'https://stprep.heroku.com/courses/',
-      cancel_url: 'https://stprep.heroku.com/courses/',
+      success_url: 'https://stprep.herokuapp.com/courses/',
+      cancel_url: 'https://stprep.herokuapp.com/courses/',
     })
     render 'new' if !!@session.id
   end
