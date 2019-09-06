5730675d3990b667d52e8acf1a1ac381ad5cb8aa
Rails Udemy Course <railsudemycourse@gmail.com>
2019-09-06 03:16:51 +0100

    https redirect change

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -14,8 +14,8 @@ class PaymentsController < ApplicationController
         currency: 'gbp',
         quantity: 1,
       }],
-      success_url: 'http://stprep.heroku.com/courses/',
-      cancel_url: 'http://stprep.heroku.com/courses/',
+      success_url: 'https://stprep.heroku.com/courses/',
+      cancel_url: 'https://stprep.heroku.com/courses/',
     })
     render 'new' if !!@session.id
   end
