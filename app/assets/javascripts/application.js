a963158a5d5ea7569e6abca8483c5410509959c9
Rails Udemy Course <railsudemycourse@gmail.com>
2019-09-06 14:08:06 +0100

    scroll animated main-nav-bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,13 @@ window.setTimeout(function() {
         $(this).remove();
     });
 }, 2000);
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("main-nav-bar").style.padding = "1em";
+  } else {
+    document.getElementById("main-nav-bar").style.padding = "1.5em 2.5em 1.5em 2.5em";
+  }
+}
