a167eff3f25232434cee62d5aedace9fe294bac9
Rails Udemy Course <railsudemycourse@gmail.com>
2019-09-17 22:58:39 +0100

    more front end

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,15 +1,20 @@
 class PaymentsController < ApplicationController
   require 'stripe'
+
+  def index
+    redirect_to courses_path
+  end
+
   def create
-    course = Course.find_by_id(params[:course])
+    @course = Course.find_by_id(params[:course])
     user = current_user
     @session = Stripe::Checkout::Session.create({
       payment_method_types: ['card'],
       customer_email: user.email,
-      client_reference_id: course.id,
+      client_reference_id: @course.id,
       line_items: [{
-        name: course.title,
-        description: course.description,
+        name: @course.title,
+        description: @course.description,
         amount: 500,
         currency: 'gbp',
         quantity: 1,
